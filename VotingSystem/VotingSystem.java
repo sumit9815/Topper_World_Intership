6920eff57f202b18332e15ba604b48ef813c51d2
sumit <145683227+sumit9815@users.noreply.github.com>
2023-09-24 06:33:27 +0100

    Update VotingSystem.java

--- VotingSystem/VotingSystem.java
+++ VotingSystem/VotingSystem.java
@@ -5,199 +5,116 @@ import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Atm  {
-    private Map<String, Account> accounts = new HashMap<>();
-    private Account currentUser;
-
+public class VotingSystem {
     private JFrame frame;
-    private JTextField accountNumberField;
-    private JPasswordField pinField;
-    private JLabel messageLabel;
+    private Map<String, String> users;
+    private Map<String, String> votes;
+    private String currentUser;
 
-    public Atm () {
-        initializeAccounts();
-
-        frame = new JFrame("ATM Interface");
-        frame.setSize(400, 200);
+    public VotingSystemGUI() {
+        frame = new JFrame("Online Voting System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
         frame.setLayout(new BorderLayout());
 
-        JPanel loginPanel = new JPanel(new GridLayout(3, 2));
-        JLabel accountNumberLabel = new JLabel("Account Number:");
-        JLabel pinLabel = new JLabel("PIN:");
-        accountNumberField = new JTextField();
-        pinField = new JPasswordField();
+        users = new HashMap<>();
+        votes = new HashMap<>();
+
+        JPanel loginPanel = new JPanel(new GridLayout(4, 1));
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+
         JButton loginButton = new JButton("Login");
-        messageLabel = new JLabel("");
+        JLabel statusLabel = new JLabel();
 
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String accountNumber = accountNumberField.getText();
-                String pin = new String(pinField.getPassword());
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
 
-                if (authenticate(accountNumber, pin)) {
-                    showTransactionOptions();
+                if (authenticateUser(username, password)) {
+                    statusLabel.setText("Login successful.");
+                    currentUser = username;
+                    createVotingPanel();
                 } else {
-                    messageLabel.setText("Authentication failed. Please try again.");
+                    statusLabel.setText("Login failed. Invalid username or password.");
+                }
+
+                usernameField.setText("");
+                passwordField.setText("");
+            }
+        });
+
+        loginPanel.add(usernameLabel);
+        loginPanel.add(usernameField);
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordField);
+        loginPanel.add(loginButton);
+        loginPanel.add(statusLabel);
+
+        frame.add(loginPanel, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
+
+    private boolean authenticateUser(String username, String password) {
+        // Simulated user authentication (replace with your own logic)
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void createVotingPanel() {
+        frame.getContentPane().removeAll();
+        frame.revalidate();
+        frame.repaint();
+
+        JPanel votingPanel = new JPanel(new GridLayout(4, 1));
+
+        JLabel welcomeLabel = new JLabel("Welcome, " + currentUser + "!");
+        JLabel selectCandidateLabel = new JLabel("Select a candidate:");
+
+        JComboBox<String> candidateComboBox = new JComboBox<>(new String[]{"Candidate 1", "Candidate 2", "Candidate 3"});
+
+        JButton voteButton = new JButton("Vote");
+        JLabel voteStatus = new JLabel();
+
+        voteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedCandidate = (String) candidateComboBox.getSelectedItem();
+                if (selectedCandidate != null) {
+                    votes.put(currentUser, selectedCandidate);
+                    voteStatus.setText("Vote cast successfully for " + selectedCandidate);
+                    candidateComboBox.setEnabled(false);
+                    voteButton.setEnabled(false);
+                } else {
+                    voteStatus.setText("Please select a candidate.");
                 }
             }
         });
 
-        loginPanel.add(accountNumberLabel);
-        loginPanel.add(accountNumberField);
-        loginPanel.add(pinLabel);
-        loginPanel.add(pinField);
-        loginPanel.add(new JLabel()); // Empty cell for spacing
-        loginPanel.add(loginButton);
+        votingPanel.add(welcomeLabel);
+        votingPanel.add(selectCandidateLabel);
+        votingPanel.add(candidateComboBox);
+        votingPanel.add(voteButton);
+        votingPanel.add(voteStatus);
 
-        frame.add(loginPanel, BorderLayout.CENTER);
-        frame.add(messageLabel, BorderLayout.SOUTH);
-        frame.setVisible(true);
+        frame.add(votingPanel, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Atm();
+                new VotingSystemGUI();
             }
         });
-    }
-
-    private void initializeAccounts() {
-        // Create sample accounts
-        Account account1 = new Account("12345", "1234", 1000.0);
-        Account account2 = new Account("67890", "5678", 500.0);
-
-        accounts.put(account1.getAccountNumber(), account1);
-        accounts.put(account2.getAccountNumber(), account2);
-    }
-
-    private boolean authenticate(String accountNumber, String pin) {
-        Account account = accounts.get(accountNumber);
-        if (account != null && account.getPin().equals(pin)) {
-            currentUser = account;
-            return true;
-        }
-        return false;
-    }
-
-    private void showTransactionOptions() {
-        frame.getContentPane().removeAll();
-        frame.revalidate();
-        frame.repaint();
-
-        JPanel transactionPanel = new JPanel(new GridLayout(4, 1));
-        JLabel titleLabel = new JLabel("Choose Transaction:");
-        JButton checkBalanceButton = new JButton("Check Balance");
-        JButton depositButton = new JButton("Deposit");
-        JButton withdrawButton = new JButton("Withdraw");
-        JButton logoutButton = new JButton("Logout");
-
-        checkBalanceButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                showMessage("Your balance: $" + currentUser.getBalance());
-            }
-        });
-
-        depositButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String depositAmount = JOptionPane.showInputDialog(frame, "Enter the deposit amount:");
-                try {
-                    double amount = Double.parseDouble(depositAmount);
-                    if (amount > 0) {
-                        currentUser.deposit(amount);
-                        showMessage("Deposit successful. Your new balance: $" + currentUser.getBalance());
-                    } else {
-                        showMessage("Invalid amount. Please enter a positive number.");
-                    }
-                } catch (NumberFormatException ex) {
-                    showMessage("Invalid amount. Please enter a valid number.");
-                }
-            }
-        });
-
-        withdrawButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String withdrawAmount = JOptionPane.showInputDialog(frame, "Enter the withdrawal amount:");
-                try {
-                    double amount = Double.parseDouble(withdrawAmount);
-                    if (amount > 0 && currentUser.getBalance() >= amount) {
-                        currentUser.withdraw(amount);
-                        showMessage("Withdrawal successful. Your new balance: $" + currentUser.getBalance());
-                    } else if (amount <= 0) {
-                        showMessage("Invalid amount. Please enter a positive number.");
-                    } else {
-                        showMessage("Insufficient balance.");
-                    }
-                } catch (NumberFormatException ex) {
-                    showMessage("Invalid amount. Please enter a valid number.");
-                }
-            }
-        });
-
-        logoutButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentUser = null;
-                frame.getContentPane().removeAll();
-                frame.revalidate();
-                frame.repaint();
-                initializeGUI();
-            }
-        });
-
-        transactionPanel.add(titleLabel);
-        transactionPanel.add(checkBalanceButton);
-        transactionPanel.add(depositButton);
-        transactionPanel.add(withdrawButton);
-        transactionPanel.add(logoutButton);
-
-        frame.add(transactionPanel, BorderLayout.CENTER);
-        frame.add(messageLabel, BorderLayout.SOUTH);
-        frame.revalidate();
-    }
-
-    protected void initializeGUI() {
-    }
-
-    private void showMessage(String message) {
-        JOptionPane.showMessageDialog(frame, message);
-    }
-
-    private class Account {
-        private String accountNumber;
-        private String pin;
-        private double balance;
-
-        public Account(String accountNumber, String pin, double balance) {
-            this.accountNumber = accountNumber;
-            this.pin = pin;
-            this.balance = balance;
-        }
-
-        public String getAccountNumber() {
-            return accountNumber;
-        }
-
-        public String getPin() {
-            return pin;
-        }
-
-        public double getBalance() {
-            return balance;
-        }
-
-        public void deposit(double amount) {
-            balance += amount;
-        }
-
-        public void withdraw(double amount) {
-            balance -= amount;
-        }
     }
 }
